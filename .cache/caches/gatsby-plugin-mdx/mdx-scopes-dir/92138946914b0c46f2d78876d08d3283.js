5180645303df28633eec202ea4f9ae50e42eddb5
abdelmounaim bougaze <abdelmounaim.bougaze@gmail.com>
2020-06-09 03:33:41 +0000

    Updates

--- .cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/92138946914b0c46f2d78876d08d3283.js
+++
@@ -1,8 +0,0 @@
-import { mix } from '@theme-ui/color';
-import Footer from "..\\..\\..\\..\\src\\components\\footer";
-import * as React from 'react';
-export default {
-  mix,
-  Footer,
-  React
-};
